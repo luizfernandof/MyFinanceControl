55b4b12e7e95bef63f66cbde81c02ee476837492
Luiz Fernando F. Silva <luizfernandofs@protonmail.com>
2026-01-14 17:59:28 -0300

    feat: create initial entities and relationships

---
+++ src/main/java/br/com/devl/mfc/entity/Transaction.java
@@ -0,0 +1,105 @@
+package br.com.devl.mfc.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import br.com.devl.mfc.auth.entity.User;
+import br.com.devl.mfc.enums.TransactionType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String description;
+	
+	@Column(nullable = false, precision = 15, scale = 2)
+	private BigDecimal amount;
+	
+	@Column(nullable = false)
+	private LocalDate date;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private TransactionType type;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "category_id", nullable = false)
+	private Category category;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public TransactionType getType() {
+		return type;
+	}
+
+	public void setType(TransactionType type) {
+		this.type = type;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+}
---
+++ src/main/java/br/com/devl/mfc/enums/CategoryType.java
@@ -0,0 +1,5 @@
+package br.com.devl.mfc.enums;
+
+public enum CategoryType {
+	INCOME, EXPENSE;
+}
