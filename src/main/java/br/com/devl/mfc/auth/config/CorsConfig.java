11a256e83937d32d453509565e2eb27f96a2117c
LuizFernandoF <luizfernandofs@protonmail.com>
2026-04-08 18:40:14 +0000

    fix: adjust CORS config

--- src/main/java/br/com/devl/mfc/auth/config/CorsConfig.java
+++ src/main/java/br/com/devl/mfc/auth/config/CorsConfig.java
@@ -14,7 +14,12 @@ public class CorsConfig {
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5173"));
+		configuration.setAllowedOrigins(List.of(
+			"http://localhost:5173",
+			"http://127.0.0.1:5173",
+			"http://mfc.devl.com.br",
+			"https://mfc.devl.com.br"
+		));
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("*"));
 		configuration.setAllowCredentials(true);
